07a768373247b13735e4936f06fba3e57c04e786
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-05-19 13:19:59 +0300

    implemented th auth client and google sign in functionality on the sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const page = () => {
+  const handleSign = async () => {
+    return await authClient.signIn.social({ provider: "google" });
+  };
   return (
     <main className="sign-in">
       <aside className="testimonial">
@@ -73,7 +79,7 @@ const page = () => {
             time
           </p>
 
-          <button>
+          <button onClick={handleSign}>
             <Image
               src="/assets/icons/google.svg"
               alt="google"
